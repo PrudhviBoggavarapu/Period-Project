2f3d18a80e39426e9f2285892b024a7fb9488336
PrudhviBoggavarapu <codingpabs@gmail.com>
2024-02-02 20:44:25 -0600

    Fixed background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,8 @@
-<h1 class="text-2xl font-bold underline">Main Dashboard</h1>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
 <script>
-	import { goto } from '$app/navigation';
-		import { Button } from '$lib/components/ui/button';
+	import { Button } from '$lib/components/ui/button';
 </script>
+
+<h1 class="text-2xl font-bold underline">Main Dashboard</h1>
 <div class="flex justify-center items-center flex-col pb-6">
-	<Button
-		variant="outline"
-		on:click={() => {
-			localStorage.setItem('SawWelcome', 'true');
-			goto('/').then(() => {
-				setTimeout(() => {
-					window.location.reload();
-				}, 0);
-			});
-		}}
-		>View Dispenser Data
-	</Button>
+	<Button>View Dispenser Data</Button>
 </div>
